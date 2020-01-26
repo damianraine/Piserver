87c9930acd32711f17ad9ced073d5637bb183553
Damian Raine <damianraine@hotmail.com>
2020-01-26 16:21:36 +0000

    added in deluge web start and cancel

--- nordvpn.sh
+++ nordvpn.sh
@@ -3,23 +3,38 @@
 #checks in the a there is a live connection to nordVPN - if not it connects
 if [[ $(sudo nordvpn status) == *"Status: Connected"* ]]; then
 
-echo "Already Connected"
+	echo "Already Connected"
 
-else 
+	 if [[ ! $(pgrep -f deluge-web) ]]; then
+                  deluge-web &
+		  deluged
+                  echo "deluge pushed live"
+          fi
+else
 
-sleep 30
+	kill $(pgrep -f deluge-web)
+	echo "deluge killed"
+	sleep 30
 
-nordvpn connect sg188
+		nordvpn connect sg188
 
-echo "Connected to NORD"
+		echo "Connected to NORD"
 
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P OUTPUT ACCEPT
-sudo iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
-sudo iptables -A FORWARD -i tun0 -o wlan0 -m state --state RELATED,ESTABLISHED $
-sudo iptables -A FORWARD -i wlan0 -o tun0 -j ACCEPT
+		sudo iptables -P INPUT ACCEPT
+		sudo iptables -P OUTPUT ACCEPT
+		sudo iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
+		sudo iptables -A FORWARD -i tun0 -o wlan0 -m state --state RELATED,ESTABLISHED $
+		sudo iptables -A FORWARD -i wlan0 -o tun0 -j ACCEPT
 
-echo "IP Tables Fixed"
+		echo "IP Tables Fixed"
+
+			if [[ ! $(pgrep -f deluge-web) ]]; then
+			 	  deluge-web &
+				  deluged
+			echo "deluge pushed live"
+			fi
+
+
 
 fi
 
